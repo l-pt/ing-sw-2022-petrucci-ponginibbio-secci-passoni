af4f4b682c44fe8f36ee63a5fbfc54f700acdce4
paolo <paolosecci98@gmail.com>
2022-03-29 16:46:06 +0200

    match shouldnt be abstract class

--- src/main/java/it/polimi/ingsw/model/Match.java
+++ src/main/java/it/polimi/ingsw/model/Match.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-public abstract class Match {
+public class Match {
     private int id;
     private List<Team> teams;
     private List<Player> playerOrder;
