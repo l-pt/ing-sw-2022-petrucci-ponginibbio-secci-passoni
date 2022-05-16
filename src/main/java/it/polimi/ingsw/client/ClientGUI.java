8ea18ad6b34a137c93e1d6e04c6d9feacbd78c42
l-pt <88390968+l-pt@users.noreply.github.com>
2022-05-16 21:35:57 +0200

    ClientGUI: edit Swing components in the event dispatcher thread and fetch server messages in the background

--- src/main/java/it/polimi/ingsw/client/ClientGUI.java
+++ src/main/java/it/polimi/ingsw/client/ClientGUI.java
@@ -1,6 +1,5 @@
 package it.polimi.ingsw.client;
 
-import com.google.gson.JsonSyntaxException;
 import it.polimi.ingsw.protocol.Message;
 import it.polimi.ingsw.protocol.message.*;
 
@@ -9,6 +8,7 @@ import java.awt.*;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.lang.reflect.InvocationTargetException;
 import java.net.Socket;
 
 public class ClientGUI extends Client{
@@ -24,96 +24,102 @@ public class ClientGUI extends Client{
         in = new InputStreamReader(socket.getInputStream());
         out = new OutputStreamWriter(socket.getOutputStream());
 
-        frame = new JFrame("Eriantys");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
+        try {
+            SwingUtilities.invokeAndWait(() -> {
+                frame = new JFrame("Eriantys");
 
-        Thread t = new Thread(() -> {
-            try {
-                lobbyLoop();
-            } catch (IOException e) {
-                closeProgram();
-            }
-        });
+                JPanel contentPane = new JPanel();
+                contentPane.setOpaque(true);
+                contentPane.setBackground(Color.WHITE);
+                contentPane.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+                frame.setContentPane(contentPane);
+
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.getContentPane().setLayout(new BorderLayout(10, 10));
+                frame.setVisible(true);
+            });
+        } catch (InterruptedException | InvocationTargetException e) {
+            closeProgram();
+        }
+        Thread t = new Thread(new ClientSocketWorker(this));
         t.start();
         try {
             t.join();
         } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
     }
 
-    private void lobbyLoop() throws IOException {
-        Message msg;
-        System.out.println("Test test lobbyLoop");
-
-        while (true) {
-            //Wait for a server message
-            try {
-                msg = readMessage();
-                System.out.println("MSG");
-            } catch (JsonSyntaxException e) {
-                System.out.println("Server sent invalid message");
-                closeProgram();
-                return;
+    public void processMessage(Message msg) {
+        //We have received a server message, check its Type to answer appropriately
+        switch (msg.getMessageId()) {
+            case ERROR -> {
+                System.out.println("Server error: " + ((ErrorMessage) msg).getError());
             }
-            //We have received a server message, check its Type to answer appropriately
-            switch (msg.getMessageId()) {
-                case ERROR -> {
-                    System.out.println("Server error: " + ((ErrorMessage) msg).getError());
-                }
-                case ASK_USERNAME -> {
-                    System.out.println("Ask usernsme");
-                    frame.getContentPane().removeAll();
-                    JTextField usernameTextField = new JTextField("Insert username");
-                    usernameTextField.addActionListener(actionEvent -> {
-                        try {
-                            sendMessage(new SetUsernameMessage(usernameTextField.getText()));
-                            name = usernameTextField.getName();
-                            frame.getContentPane().removeAll();
-                            frame.getContentPane().add(new JLabel("Waiting for other players..."));
-                        } catch (IOException e) {
-                            closeProgram();
-                        }
-                    });
-                    frame.getContentPane().add(usernameTextField);
-                    frame.pack();
-                }
-                case ASK_PLAYER_NUMBER -> {
-                    JComboBox<Integer> comboBox = new JComboBox<>(new Integer[]{2, 3, 4});
-                    frame.getContentPane().removeAll();
-                    frame.getContentPane().setLayout(new FlowLayout());
-                    frame.getContentPane().add(comboBox);
-                    JButton confirmPlayers = new JButton("Confirm");
-                    frame.getContentPane().add(confirmPlayers);
-                    confirmPlayers.addActionListener(actionEvent -> {
-                        try {
-                            sendMessage(new SetPlayerNumberMessage((Integer) comboBox.getSelectedItem()));
-                            frame.getContentPane().removeAll();
-                        } catch (IOException e) {
-                            closeProgram();
-                        }
-                    });
-                }
-                case ASK_EXPERT -> {
-                    JComboBox<Boolean> comboBox = new JComboBox<>(new Boolean[]{true, false});
-                    frame.getContentPane().setLayout(new FlowLayout());
-                    frame.getContentPane().add(comboBox);
-                    JButton confirmExpert = new JButton("Confirm");
-                    frame.getContentPane().add(confirmExpert);
-                    confirmExpert.addActionListener(actionEvent -> {
-                        try {
-                            sendMessage(new SetExpertMessage((Boolean) comboBox.getSelectedItem()));
-                            frame.getContentPane().removeAll();
-                            frame.getContentPane().add(new JLabel("Waiting for other players..."));
-                        } catch (IOException e) {
-                            closeProgram();
-                        }
-                    });
-                }
-                case UPDATE_VIEW -> {
-                    return;
-                }
+            case ASK_USERNAME -> {
+                frame.getContentPane().removeAll();
+                frame.getContentPane().add(new JLabel("Insert username"), BorderLayout.PAGE_START);
+                JTextField usernameTextField = new JTextField("");
+                usernameTextField.setPreferredSize(new Dimension());
+                usernameTextField.addActionListener(actionEvent -> {
+                    try {
+                        frame.getContentPane().removeAll();
+                        frame.getContentPane().add(new JLabel("Waiting for other players..."), BorderLayout.CENTER);
+                        frame.revalidate();
+                        frame.repaint();
+                        name = usernameTextField.getName();
+                        sendMessage(new SetUsernameMessage(usernameTextField.getText()));
+                    } catch (IOException e) {
+                        closeProgram();
+                    }
+                });
+                frame.getContentPane().add(usernameTextField, BorderLayout.CENTER);
+                frame.revalidate();
+                frame.repaint();
+            }
+            case ASK_PLAYER_NUMBER -> {
+                frame.getContentPane().removeAll();
+                frame.getContentPane().add(new JLabel("Choose player number"), BorderLayout.PAGE_START);
+                JComboBox<Integer> comboBox = new JComboBox<>(new Integer[]{2, 3, 4});
+                frame.getContentPane().add(comboBox, BorderLayout.CENTER);
+                JButton confirmPlayers = new JButton("Confirm");
+                confirmPlayers.addActionListener(actionEvent -> {
+                    try {
+                        frame.getContentPane().removeAll();
+                        frame.revalidate();
+                        frame.repaint();
+                        sendMessage(new SetPlayerNumberMessage((Integer) comboBox.getSelectedItem()));
+                    } catch (IOException e) {
+                        closeProgram();
+                    }
+                });
+                frame.getContentPane().add(confirmPlayers, BorderLayout.PAGE_END);
+                frame.revalidate();
+                frame.repaint();
+            }
+            case ASK_EXPERT -> {
+                frame.getContentPane().removeAll();
+                frame.getContentPane().add(new JLabel("Activate expert mode?"), BorderLayout.PAGE_START);
+                JComboBox<Boolean> comboBox = new JComboBox<>(new Boolean[]{true, false});
+                frame.getContentPane().add(comboBox, BorderLayout.CENTER);
+                JButton confirmExpert = new JButton("Confirm");
+                confirmExpert.addActionListener(actionEvent -> {
+                    try {
+                        frame.getContentPane().removeAll();
+                        frame.getContentPane().add(new JLabel("Waiting for other players..."), BorderLayout.CENTER);
+                        frame.revalidate();
+                        frame.repaint();
+                        sendMessage(new SetExpertMessage((Boolean) comboBox.getSelectedItem()));
+                    } catch (IOException e) {
+                        closeProgram();
+                    }
+                });
+                frame.getContentPane().add(confirmExpert, BorderLayout.PAGE_END);
+                frame.revalidate();
+                frame.repaint();
+            }
+            case UPDATE_VIEW -> {
+                //TODO
             }
         }
     }
