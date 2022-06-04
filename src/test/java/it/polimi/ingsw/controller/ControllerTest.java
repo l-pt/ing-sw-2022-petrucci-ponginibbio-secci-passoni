d419064be2907c371957cf8e7fc04a9de45cbf24
simonepongi <10699608@polimi.it>
2022-06-04 12:40:30 +0200

    add controller tests

--- src/test/java/it/polimi/ingsw/controller/ControllerTest.java
+++ src/test/java/it/polimi/ingsw/controller/ControllerTest.java
@@ -1,14 +1,11 @@
 package it.polimi.ingsw.controller;
 
 import it.polimi.ingsw.model.*;
-import it.polimi.ingsw.model.character.impl.Character1;
-import it.polimi.ingsw.model.character.impl.Character2;
+import it.polimi.ingsw.model.character.impl.*;
 import it.polimi.ingsw.protocol.Message;
 import it.polimi.ingsw.protocol.MessageId;
 import it.polimi.ingsw.protocol.message.*;
-import it.polimi.ingsw.protocol.message.character.UseCharacterColorIslandMessage;
-import it.polimi.ingsw.protocol.message.character.UseCharacterMessage;
-import it.polimi.ingsw.protocol.message.character.UseNoCharacterMessage;
+import it.polimi.ingsw.protocol.message.character.*;
 import it.polimi.ingsw.server.Server;
 import junit.framework.TestCase;
 import org.junit.jupiter.api.AfterAll;
@@ -273,7 +270,7 @@ public class ControllerTest extends TestCase {
     }
 
     @Test
-    public synchronized void lastTurnMotherNatureMessageWithCharacter() throws IllegalMoveException {
+    public synchronized void lastTurnMotherNatureMessageWithCharacter1() throws IllegalMoveException {
         server.setMatchParameters(2, true);
         List<String> names = List.of("test1", "test2");
         Controller controller = new Controller(server, names);
@@ -300,6 +297,43 @@ public class ControllerTest extends TestCase {
         result = controller.handleMessage(name1, new SetMotherNatureMessage(1));
         Assertions.assertEquals(1, result.get(name2).size());
         Assertions.assertEquals(MessageId.ASK_CHARACTER, result.get(name2).get(0).getMessageId());
+        Assertions.assertEquals((pos + player1.getCurrentAssistant().getMoves()) % 12, controller.getMatch().getPosMotherNature());
+    }
+
+    @Test
+    public synchronized void lastTurnMotherNatureMessageWithCharacter2() throws IllegalMoveException {
+        server.setMatchParameters(2, true);
+        List<String> names = List.of("test1", "test2");
+        Controller controller = new Controller(server, names);
+        String name1 = controller.getMatch().getPlayersOrder().get(0).getName();
+        Player player1 = controller.getMatch().getPlayerFromName(name1);
+        String name2 = controller.getMatch().getPlayersOrder().get(1).getName();
+        Player player2 = controller.getMatch().getPlayerFromName(name2);
+        player2.getAssistants().clear();
+        player2.getAssistants().add(new Assistant(2, 1));
+        int pos = controller.getMatch().getPosMotherNature();
+
+        Map<String, List<Message>> result = controller.handleMessage(name1, new SetAssistantMessage(1));
+        Assertions.assertEquals(1, result.get(name2).size());
+        Assertions.assertEquals(MessageId.ASK_ASSISTANT, result.get(name2).get(0).getMessageId());
+        Assertions.assertEquals(1, player1.getCurrentAssistant().getValue());
+
+        result = controller.handleMessage(name2, new SetAssistantMessage(2));
+        Assertions.assertEquals(1, result.get(name1).size());
+        Assertions.assertEquals(MessageId.ASK_CHARACTER, result.get(name1).get(0).getMessageId());
+        Assertions.assertEquals(2, player2.getCurrentAssistant().getValue());
+
+        controller.getMatch().getPlayerFromName(name2).addCoin();
+        controller.getMatch().getCharacters().set(0, new Character2());
+
+        controller.getNextMessage().put(name2, new AskMotherNatureMessage());
+        result = controller.handleMessage(name2, new UseCharacterMessage(1));
+        Assertions.assertEquals(1, result.get(name2).size());
+        Assertions.assertEquals(MessageId.ASK_MOTHER_NATURE, result.get(name2).get(0).getMessageId());
+
+        result = controller.handleMessage(name2, new SetMotherNatureMessage(1));
+        Assertions.assertEquals(1, result.get(name1).size());
+        Assertions.assertEquals(MessageId.END_GAME, result.get(name1).get(0).getMessageId());
         Assertions.assertEquals((pos + player1.getCurrentAssistant().getMoves()) % 12, controller.getMatch().getPosMotherNature());
     }
 
@@ -354,7 +388,7 @@ public class ControllerTest extends TestCase {
     }
 
     @Test
-    public synchronized void cloudMessageWithCharacter() throws IllegalMoveException {
+    public synchronized void cloudMessageWithCharacter1() throws IllegalMoveException {
         server.setMatchParameters(2, true);
         List<String> names = List.of("test1", "test2");
         Controller controller = new Controller(server, names);
@@ -376,23 +410,59 @@ public class ControllerTest extends TestCase {
     }
 
     @Test
+    public synchronized void cloudMessageWithCharacter2() throws IllegalMoveException {
+        server.setMatchParameters(2, true);
+        List<String> names = List.of("test1", "test2");
+        Controller controller = new Controller(server, names);
+        String name1 = controller.getMatch().getPlayersOrder().get(0).getName();
+        String name2 = controller.getMatch().getPlayersOrder().get(1).getName();
+        Player player1 = controller.getMatch().getPlayerFromName(name1);
+        Player player2 = controller.getMatch().getPlayerFromName(name2);
+
+        player2.addCoin();
+        controller.getMatch().getCharacters().set(0, new Character2());
+
+        Map<String, List<Message>> result = controller.handleMessage(name1, new SetAssistantMessage(1));
+        Assertions.assertEquals(1, result.get(name2).size());
+        Assertions.assertEquals(MessageId.ASK_ASSISTANT, result.get(name2).get(0).getMessageId());
+        Assertions.assertEquals(1, player1.getCurrentAssistant().getValue());
+
+        result = controller.handleMessage(name2, new SetAssistantMessage(2));
+        Assertions.assertEquals(1, result.get(name1).size());
+        Assertions.assertEquals(MessageId.ASK_CHARACTER, result.get(name1).get(0).getMessageId());
+        Assertions.assertEquals(2, player2.getCurrentAssistant().getValue());
+
+        controller.getNextMessage().put(name2, new AskCloudMessage());
+        result = controller.handleMessage(name2, new UseCharacterMessage(1));
+        Assertions.assertEquals(1, result.get(name2).size());
+        Assertions.assertEquals(MessageId.ASK_CLOUD, result.get(name2).get(0).getMessageId());
+
+        result = controller.handleMessage(name2, new SetCloudMessage(1));
+        Assertions.assertEquals(1, result.get(name1).size());
+        Assertions.assertEquals(MessageId.ASK_ASSISTANT, result.get(name1).get(0).getMessageId());
+        Assertions.assertEquals(3, controller.getMatch().getClouds().get(1).getStudents().size());
+    }
+
+    @Test
     public synchronized void characterColorIslandMessage() throws IllegalMoveException {
         server.setMatchParameters(2, true);
         List<String> names = List.of("test1", "test2");
         Controller controller = new Controller(server, names);
         String name1 = controller.getMatch().getPlayersOrder().get(0).getName();
+        Player player1 = controller.getMatch().getPlayerFromName(name1);
 
         controller.getMatch().getCharacters().set(0, new Character1());
         Character1 character1 = (Character1)controller.getMatch().getCharacters().get(0);
         character1.setup(controller.getMatch());
+        player1.removeCoins(1);
 
-        controller.getMatch().getPlayerFromName(name1).removeCoins(1);
         Map<String, List<Message>> result = controller.handleMessage(name1, new UseCharacterColorIslandMessage(character1.getStudents().get(0).getColor(), 1));
         Assertions.assertEquals(2, result.get(name1).size());
         Assertions.assertEquals("You don't have enough coins to activate this character ability", ((ErrorMessage) result.get(name1).get(0)).getError());
         Assertions.assertEquals(MessageId.ASK_CHARACTER, result.get(name1).get(1).getMessageId());
 
-        controller.getMatch().getPlayerFromName(name1).addCoin();
+        player1.addCoin();
+
         result = controller.handleMessage(name1, new UseCharacterColorIslandMessage(character1.getStudents().get(0).getColor(), 15));
         Assertions.assertEquals(2, result.get(name1).size());
         Assertions.assertEquals("Island number must be between 1 and " + controller.getMatch().getIslands().size(), ((ErrorMessage) result.get(name1).get(0)).getError());
@@ -418,6 +488,185 @@ public class ControllerTest extends TestCase {
         Assertions.assertEquals(4, character1.getStudents().size());
         Assertions.assertEquals(color1, controller.getMatch().getIslands().get(1).getStudents().get(controller.getMatch().getIslands().get(1).getStudents().size() - 1).getColor());
     }
+
+    @Test
+    public synchronized void lastMessageCharacterColorIslandMessage() throws IllegalMoveException {
+        server.setMatchParameters(2, true);
+        List<String> names = List.of("test1", "test2");
+        Controller controller = new Controller(server, names);
+        String name1 = controller.getMatch().getPlayersOrder().get(0).getName();
+        String name2 = controller.getMatch().getPlayersOrder().get(1).getName();
+
+        controller.getMatch().getCharacters().set(0, new Character1());
+        Character1 character1 = (Character1)controller.getMatch().getCharacters().get(0);
+        character1.setup(controller.getMatch());
+        PawnColor color1 = character1.getStudents().get(0).getColor();
+
+        Map<String, List<Message>> result = controller.handleMessage(name1, new SetCloudMessage(1));
+        Assertions.assertEquals(1, result.get(name1).size());
+        Assertions.assertEquals(MessageId.ASK_CHARACTER, result.get(name1).get(0).getMessageId());
+        Assertions.assertEquals(0, controller.getMatch().getClouds().get(1).getStudents().size());
+
+        result = controller.handleMessage(name1, new UseCharacterColorIslandMessage(color1, 1));
+        Assertions.assertEquals(1, result.get(name2).size());
+        Assertions.assertEquals(MessageId.ASK_CHARACTER, result.get(name2).get(0).getMessageId());
+        Assertions.assertEquals(4, character1.getStudents().size());
+        Assertions.assertEquals(color1, controller.getMatch().getIslands().get(1).getStudents().get(controller.getMatch().getIslands().get(1).getStudents().size() - 1).getColor());
+    }
+
+    @Test
+    public synchronized void characterColorMessage() throws IllegalMoveException {
+        server.setMatchParameters(2, true);
+        List<String> names = List.of("test1", "test2");
+        Controller controller = new Controller(server, names);
+        String name1 = controller.getMatch().getPlayersOrder().get(0).getName();
+        Player player1 = controller.getMatch().getPlayerFromName(name1);
+        controller.getMatch().getCharacters().set(0, new Character9());
+
+        Map<String, List<Message>> result = controller.handleMessage(name1, new UseCharacterColorMessage(8, PawnColor.RED));
+        Assertions.assertEquals(2, result.get(name1).size());
+        Assertions.assertEquals("You don't have enough coins to activate this character ability", ((ErrorMessage) result.get(name1).get(0)).getError());
+        Assertions.assertEquals(MessageId.ASK_CHARACTER, result.get(name1).get(1).getMessageId());
+
+        player1.addCoin();
+        player1.addCoin();
+
+        controller.getNextMessage().put(name1, new AskCloudMessage());
+        result = controller.handleMessage(name1, new UseCharacterColorMessage(8, PawnColor.RED));
+        Assertions.assertEquals(1, result.get(name1).size());
+        Assertions.assertEquals(MessageId.ASK_CLOUD, result.get(name1).get(0).getMessageId());
+        Assertions.assertEquals(PawnColor.RED, controller.getMatch().getInfluencePolicy().getExcludedColor());
+    }
+
+    @Test
+    public synchronized void lastMessageCharacterColorMessage() throws IllegalMoveException {
+        server.setMatchParameters(2, true);
+        List<String> names = List.of("test1", "test2");
+        Controller controller = new Controller(server, names);
+        String name1 = controller.getMatch().getPlayersOrder().get(0).getName();
+        Player player1 = controller.getMatch().getPlayerFromName(name1);
+        String name2 = controller.getMatch().getPlayersOrder().get(1).getName();
+        controller.getMatch().getCharacters().set(0, new Character9());
+        player1.addCoin();
+        player1.addCoin();
+
+        Map<String, List<Message>> result = controller.handleMessage(name1, new SetCloudMessage(1));
+        Assertions.assertEquals(1, result.get(name1).size());
+        Assertions.assertEquals(MessageId.ASK_CHARACTER, result.get(name1).get(0).getMessageId());
+        Assertions.assertEquals(0, controller.getMatch().getClouds().get(1).getStudents().size());
+
+        result = controller.handleMessage(name1, new UseCharacterColorMessage(8, PawnColor.RED));
+        Assertions.assertEquals(1, result.get(name2).size());
+        Assertions.assertEquals(MessageId.ASK_CHARACTER, result.get(name2).get(0).getMessageId());
+    }
+
+    @Test
+    public synchronized void lastMessageCharacterMessage() throws IllegalMoveException {
+        server.setMatchParameters(2, true);
+        List<String> names = List.of("test1", "test2");
+        Controller controller = new Controller(server, names);
+        String name1 = controller.getMatch().getPlayersOrder().get(0).getName();
+        Player player1 = controller.getMatch().getPlayerFromName(name1);
+        String name2 = controller.getMatch().getPlayersOrder().get(1).getName();
+        controller.getMatch().getCharacters().set(0, new Character2());
+
+        Map<String, List<Message>> result = controller.handleMessage(name1, new UseCharacterMessage(1));
+        Assertions.assertEquals(2, result.get(name1).size());
+        Assertions.assertEquals("You don't have enough coins to activate this character ability", ((ErrorMessage) result.get(name1).get(0)).getError());
+        Assertions.assertEquals(MessageId.ASK_CHARACTER, result.get(name1).get(1).getMessageId());
+
+        player1.addCoin();
+
+        result = controller.handleMessage(name1, new SetCloudMessage(1));
+        Assertions.assertEquals(1, result.get(name1).size());
+        Assertions.assertEquals(MessageId.ASK_CHARACTER, result.get(name1).get(0).getMessageId());
+        Assertions.assertEquals(0, controller.getMatch().getClouds().get(1).getStudents().size());
+
+        result = controller.handleMessage(name1, new UseCharacterMessage(1));
+        Assertions.assertEquals(1, result.get(name2).size());
+        Assertions.assertEquals(MessageId.ASK_CHARACTER, result.get(name2).get(0).getMessageId());
+    }
+
+    @Test
+    public synchronized void characterIslandMessage() throws IllegalMoveException {
+        server.setMatchParameters(2, true);
+        List<String> names = List.of("test1", "test2");
+        Controller controller = new Controller(server, names);
+        String name1 = controller.getMatch().getPlayersOrder().get(0).getName();
+        Player player1 = controller.getMatch().getPlayerFromName(name1);
+        controller.getMatch().getCharacters().set(0, new Character5());
+
+        Map<String, List<Message>> result = controller.handleMessage(name1, new UseCharacterIslandMessage(4, 15));
+        Assertions.assertEquals(2, result.get(name1).size());
+        Assertions.assertEquals("Island must be between 1 and " + controller.getMatch().getIslands().size(), ((ErrorMessage) result.get(name1).get(0)).getError());
+        Assertions.assertEquals(MessageId.ASK_CHARACTER, result.get(name1).get(1).getMessageId());
+
+        result = controller.handleMessage(name1, new UseCharacterIslandMessage(4, 1));
+        Assertions.assertEquals(2, result.get(name1).size());
+        Assertions.assertEquals("You don't have enough coins to activate this character ability", ((ErrorMessage) result.get(name1).get(0)).getError());
+        Assertions.assertEquals(MessageId.ASK_CHARACTER, result.get(name1).get(1).getMessageId());
+
+        player1.addCoin();
+
+        controller.getNextMessage().put(name1, new AskCloudMessage());
+        result = controller.handleMessage(name1, new UseCharacterIslandMessage(4, 1));
+        Assertions.assertEquals(1, result.get(name1).size());
+        Assertions.assertEquals(MessageId.ASK_CLOUD, result.get(name1).get(0).getMessageId());
+        Assertions.assertEquals(1, controller.getMatch().getIslands().get(1).getNoEntry());
+    }
+
+    @Test
+    public synchronized void endGameCharacterIslandMessage() throws IllegalMoveException {
+        server.setMatchParameters(2, true);
+        List<String> names = List.of("test1", "test2");
+        Controller controller = new Controller(server, names);
+        String name1 = controller.getMatch().getPlayersOrder().get(0).getName();
+        Player player1 = controller.getMatch().getPlayerFromName(name1);
+        Team team1 = controller.getMatch().getTeamFromPlayer(player1);
+        controller.getMatch().getCharacters().set(0, new Character3());
+        player1.addCoin();
+        player1.addCoin();
+        int pos = controller.getMatch().getPosMotherNature();
+
+        team1.getTowers().clear();
+        team1.addTower(new Tower(team1.getTowerColor()));
+
+        Assertions.assertEquals(1, team1.getTowers().size());
+
+        PawnColor color = player1.getSchool().getEntrance().get(0).getColor();
+        controller.getMatch().playerMoveStudent(name1, color);
+        Assertions.assertTrue(player1.getSchool().isColoredProfessor(color));
+        controller.getMatch().getIslands().get(1).addStudent(new Student(color));
+
+        Map<String, List<Message>> result = controller.handleMessage(name1, new UseCharacterIslandMessage(2, 1));
+        Assertions.assertEquals(1, result.get(name1).size());
+        Assertions.assertEquals(MessageId.END_GAME, result.get(name1).get(0).getMessageId());
+        Assertions.assertEquals(player1, ((EndGameMessage)result.get(name1).get(0)).getWinner().getPlayers().get(0));
+        Assertions.assertEquals(pos, controller.getMatch().getPosMotherNature());
+    }
+
+    @Test
+    public synchronized void lastMessageCharacterIslandMessage() throws IllegalMoveException {
+        server.setMatchParameters(2, true);
+        List<String> names = List.of("test1", "test2");
+        Controller controller = new Controller(server, names);
+        String name1 = controller.getMatch().getPlayersOrder().get(0).getName();
+        Player player1 = controller.getMatch().getPlayerFromName(name1);
+        String name2 = controller.getMatch().getPlayersOrder().get(1).getName();
+        controller.getMatch().getCharacters().set(0, new Character5());
+        player1.addCoin();
+
+        Map<String, List<Message>> result = controller.handleMessage(name1, new SetCloudMessage(1));
+        Assertions.assertEquals(1, result.get(name1).size());
+        Assertions.assertEquals(MessageId.ASK_CHARACTER, result.get(name1).get(0).getMessageId());
+        Assertions.assertEquals(0, controller.getMatch().getClouds().get(1).getStudents().size());
+
+        result = controller.handleMessage(name1, new UseCharacterIslandMessage(4, 1));
+        Assertions.assertEquals(1, result.get(name2).size());
+        Assertions.assertEquals(MessageId.ASK_CHARACTER, result.get(name2).get(0).getMessageId());
+        Assertions.assertEquals(1, controller.getMatch().getIslands().get(1).getNoEntry());
+    }
+
 
     @AfterAll
     public static void closeServer(){
