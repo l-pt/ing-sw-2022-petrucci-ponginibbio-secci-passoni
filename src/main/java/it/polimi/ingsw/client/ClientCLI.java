bdecf01c937e84b45b6b8002ca651c8df69a64dd
teopa <matteo1.passoni@mail.polimi.it>
2022-06-17 14:39:09 +0200

    CLI changes

--- src/main/java/it/polimi/ingsw/client/ClientCLI.java
+++ src/main/java/it/polimi/ingsw/client/ClientCLI.java
@@ -123,7 +123,7 @@ public class ClientCLI extends Client{
 
         switch (msg.getMessageId()) {
             case ASK_ASSISTANT -> {
-                int assistant = readInt("What assistant do you want to play?");
+                int assistant = readInt("\u001b[53;1HWhat assistant do you want to play?");
                 sendMessage(new SetAssistantMessage(assistant));
             }
             case ASK_ENTRANCE_STUDENT -> {
