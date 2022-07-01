38cf9a6e91ea02c3605a0ccb29922fcbf5e6e94f
simonepongi <10699608@polimi.it>
2022-07-01 10:14:50 +0200

    add comments

--- src/main/java/it/polimi/ingsw/client/ClientGUIApp.java
+++ src/main/java/it/polimi/ingsw/client/ClientGUIApp.java
@@ -1,21 +1,11 @@
 package it.polimi.ingsw.client;
 
-import it.polimi.ingsw.client.Client;
-import it.polimi.ingsw.client.ClientGUI;
-
-import java.io.IOException;
-
 public class ClientGUIApp {
     /**
-     *
-     * @param args
+     * Main Method of ClientGUIApp
+     * @param args Terminal argument
      */
     public static void main(String[] args){
-        Client client = new ClientGUI();
-        try{
-            client.run();
-        }catch (IOException e){
-            System.err.println(e.getMessage());
-        }
+        new ClientGUI().run();
     }
 }
